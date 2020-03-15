ce125009ae399382635bbf553dc5767d851b103b
Grzegorz Węgliński <grzegorz.weglinski@external.milliporesigma.com>
2020-03-15 14:36:48 +0100

    Create bare users module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { EffectsModule } from '@ngrx/effects';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    UsersModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
   ],
